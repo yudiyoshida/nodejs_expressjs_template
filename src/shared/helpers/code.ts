ab405b5876eb9c54dfe10f81c7a878e52867a158
Yudi Matsuguma <yudimatsuguma@gmail.com>
2023-08-10 00:35:51 -0300

    tests: criado novos tests para a classe CodeHelper

--- src/shared/helpers/code.ts
+++ src/shared/helpers/code.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto';
 
 class CodeHelper {
-  public generate(minutes: number) {
+  public generate(minutes: number): { code: string, codeExpiresIn: Date } {
     const now = new Date();
     const code = crypto.randomInt(10000).toString().padStart(4, '0'); // generates random number between 0000 and 9999.
     const codeExpiresIn = new Date(now.setMinutes(now.getMinutes() + minutes));
@@ -9,7 +9,7 @@ class CodeHelper {
     return { code, codeExpiresIn };
   }
 
-  public isExpired(codeExpiresIn: Date) {
+  public isExpired(codeExpiresIn: Date): boolean {
     const now = new Date();
     return (codeExpiresIn.getTime() - now.getTime() <= 0);
   }
--- tests/src/shared/helpers/code.test.ts
+++ tests/src/shared/helpers/code.test.ts
@@ -1,29 +1,57 @@
-// import CodeHelper from '../../../../src/shared/helpers/code';
+import CodeHelper from '../../../../src/shared/helpers/code';
 
-// test('it should return a code with 4 characters', () => {
-//   const { code } = CodeHelper.generate(10);
+describe('generate method', () => {
+  test('it should return a code of type string', () => {
+    const { code } = CodeHelper.generate(0);
 
-//   expect(typeof code).toBe('string');
-//   expect(code.length).toBe(4);
-// });
+    expect(typeof code).toBe('string');
+  });
 
-// test('it should return a code that expires in 10 minutes', () => {
-//   const { codeExpiresIn } = CodeHelper.generate(10);
-//   const now = new Date();
+  test('it should return a code with length equals 4', () => {
+    const { code } = CodeHelper.generate(0);
 
-//   expect(codeExpiresIn.getMinutes()).toEqual(new Date(now.setMinutes(now.getMinutes() + 10)).getMinutes());
-// });
+    expect(code).toHaveLength(4);
+  });
 
-// test('it should return false (sending valid code)', () => {
-//   const now = new Date();
-//   const validCode = new Date(now.setMinutes(now.getMinutes() + 10));
+  test('it should return a string code that contains only numbers', () => {
+    const { code } = CodeHelper.generate(0);
 
-//   expect(CodeHelper.isExpired(validCode)).toBe(false);
-// });
+    expect(Number(code)).not.toBeNaN();
+  });
 
-// test('it should return true (sending expired code)', () => {
-//   const now = new Date();
-//   const validCode = new Date(now.setMinutes(now.getMinutes() - 10));
+  test('it should return a codeExpiresIn of type date', () => {
+    const { codeExpiresIn } = CodeHelper.generate(0);
 
-//   expect(CodeHelper.isExpired(validCode)).toBe(true);
-// });
+    expect(codeExpiresIn).toBeInstanceOf(Date);
+  });
+
+  test('it should return a code that expires in 10 minutes', () => {
+    const { codeExpiresIn } = CodeHelper.generate(10);
+    const now = new Date();
+    const tenMinutesLater = new Date(now.setMinutes(now.getMinutes() + 10));
+
+    expect(codeExpiresIn.getMinutes()).toBe(tenMinutesLater.getMinutes());
+  });
+});
+
+describe('isExpired method', () => {
+  test('it should return false when sending a future date', () => {
+    const now = new Date();
+    const futureDate = new Date(now.setMinutes(now.getMinutes() + 10));
+
+    expect(CodeHelper.isExpired(futureDate)).toBe(false);
+  });
+
+  test('it should return true when sending the current date', () => {
+    const now = new Date();
+
+    expect(CodeHelper.isExpired(now)).toBe(true);
+  });
+
+  test('it should return true when sending a past date)', () => {
+    const now = new Date();
+    const pastDate = new Date(now.setMinutes(now.getMinutes() - 10));
+
+    expect(CodeHelper.isExpired(pastDate)).toBe(true);
+  });
+});
